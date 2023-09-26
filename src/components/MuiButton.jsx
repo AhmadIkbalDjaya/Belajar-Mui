8fe502018b05137238aa6702a5f5beaf233bdcbd
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-26 19:13:09 +0800

    Button Group

--- src/components/MuiButton.jsx
+++ src/components/MuiButton.jsx
@@ -1,6 +1,5 @@
-import { Stack, Button, IconButton } from "@mui/material";
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import { Send } from "@mui/icons-material";
 
 export const MuiButton = () => {
   return (
@@ -63,6 +62,20 @@ export const MuiButton = () => {
         <IconButton aria-label="send" color="success" size="small">
           <SendIcon />
         </IconButton>
+      </Stack>
+
+      <Stack direction={"row"}>
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-label="alignment button group"
+        >
+          <Button onClick={() => alert("Left Button")}>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
       </Stack>
     </Stack>
   );
