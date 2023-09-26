8b36de5e5f53c6f0afc96592ca91e5607e8dd7d7
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-09-27 03:33:42 +0800

    7-Text Field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css"
-import { MuiButton } from "./components/MuiButton";
+import "./App.css";
+import { MuiTextField } from "./components/MuiTextField";
+// import { MuiButton } from "./components/MuiButton";
 // import { MuiTypography } from "./components/MuiTypography";
 
 export default function App() {
   return (
     <>
       {/* <MuiTypography /> */}
-      <MuiButton />
+      {/* <MuiButton /> */}
+      <MuiTextField />
     </>
   );
 }
