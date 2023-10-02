51a95dc8a36147d105ac91c420cbb6e393413def
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-10-03 05:33:27 +0800

    18-Card

---
+++ src/components/MuiCard.jsx
@@ -0,0 +1,39 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+} from "@mui/material";
+
+export const MuiCard = () => {
+  return (
+    <Box width={"300px"}>
+      <Card>
+        <CardMedia
+          component={"img"}
+          height={"140"}
+          image="https://picsum.photos/200"
+          alt="Random Image"
+        />
+        <CardContent>
+          <Typography variant="h5" component={"div"}>
+            React
+          </Typography>
+          <Typography variant="body2" color={"text.secondary"}>
+            React lets you build user interfaces out of individual pieces called
+            components. Create your own React components like Thumbnail,
+            LikeButton, and Video. Then combine them into entire screens, pages,
+            and apps.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
