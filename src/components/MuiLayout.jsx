918fd534406be41c95091317d6890cc9d812c196
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-10-02 21:36:26 +0800

    17-Paper

--- src/components/MuiLayout.jsx
+++ src/components/MuiLayout.jsx
@@ -1,8 +1,8 @@
-import { Box, Stack, Divider, Grid } from "@mui/material";
+import { Box, Stack, Divider, Grid, Paper } from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
+    <Paper sx={{ padding: "32px" }} elevation={4}>
       <Stack
         sx={{ border: "1px solid" }}
         direction={"row"}
@@ -36,18 +36,26 @@ export const MuiLayout = () => {
 
       <Grid container my={4} rowSpacing={2} columnSpacing={1}>
         <Grid item xs={12} sm={6}>
-          <Box bgcolor={"primary.light"} p={2}>Item 1</Box>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 1
+          </Box>
         </Grid>
         <Grid item xs={12} sm={"auto"}>
-          <Box bgcolor={"primary.light"} p={2}>Item 2</Box>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 2
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Box bgcolor={"primary.light"} p={2}>Item 3</Box>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 3
+          </Box>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Box bgcolor={"primary.light"} p={2}>Item 4</Box>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 4
+          </Box>
         </Grid>
       </Grid>
-    </>
+    </Paper>
   );
 };
