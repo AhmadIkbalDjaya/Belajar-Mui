be2687a116eb06a91b85bf46cfbb77b524c53975
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-10-03 19:10:09 +0800

    22-Menu

--- src/components/MuiNavbar.jsx
+++ src/components/MuiNavbar.jsx
@@ -5,10 +5,22 @@ import {
   Typography,
   Stack,
   Button,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useState } from "react";
 
 export const MuiNavbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar>
       <Toolbar>
@@ -22,8 +34,39 @@ export const MuiNavbar = () => {
           <Button color="inherit">Features</Button>
           <Button color="inherit">Pricing</Button>
           <Button color="inherit">About</Button>
+          <Button
+            color="inherit"
+            id="resources-button"
+            onClick={handleClick}
+            aria-controls={open ? "resources-menu" : undefined}
+            aria-haspopup={true}
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            Resources
+          </Button>
           <Button color="inherit">Login</Button>
         </Stack>
+        <Menu
+          id="resources-item"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{
+            "aria-labelledby": "resources-button",
+          }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcash</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
